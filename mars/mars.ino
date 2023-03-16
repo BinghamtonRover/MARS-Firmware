6e70599b9c0c74617a9942efdb033394e8aa77e3
Levi Lesches <levilesches@gmail.com>
2023-03-16 00:13:03 -0400

    Update mars.ino with example code
    This won't compile or do the right thing, but it's a start

--- mars/mars.ino
+++ mars/mars.ino
@@ -1,5 +1,26 @@
 #include "tmc/src/BURT_TMC.h"
-#include "utils/src/BURT_utils.h"
+#include "utils/src/BURT_serial.h"
 
-void setup() { }
-void loop() { }
+StepperMotor motor(1, 2, 3, 4, 5, 6, 7, 8, 9, "Motor");
+BurtSerial serial(handler);
+
+float getAngle(GpsCoordinates coordinates) { } 
+
+void handler(const uint8_t* data, int length) {
+  // do something with the data
+  auto coordinates = BurtProto::decode<GpsCoordinates>(data, length, GpsCoordinates_fields);
+  float angle = getAngle(coordinates);
+  motor.moveTo(angle);
+  // ...
+}
+
+void setup() { 
+  motor.presetup();
+  motor.setup();
+} 
+
+void loop() {
+  motor.update();
+  serial.update();
+  delay(10);
+}
