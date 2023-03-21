a934a54c8a127105b78a614d633eb49d6ddcde4c
Levi Lesches <levilesches@gmail.com>
2023-03-21 14:25:59 -0700

    Properly submoduled both libraries (#2)
    * Properly submoduled Firmware-Utilities
    
    * Added TMC library
    
    * Included Protobuf library and generated code

--- mars/mars.ino
+++ mars/mars.ino
@@ -1,14 +1,16 @@
-#include "tmc/src/BURT_TMC.h"
-#include "utils/src/BURT_serial.h"
+#include "src/tmc/BURT_TMC.h"
+#include "src/utils/BURT_utils.h"
+#include "src/gps.pb.h"
 
 StepperMotor motor(1, 2, 3, 4, 5, 6, 7, 8, 9, "Motor");
 BurtSerial serial(handler);
 
-float getAngle(GpsCoordinates coordinates) { } 
+float getAngle(GpsCoordinates coordinates) { return 0; } 
 
 void handler(const uint8_t* data, int length) {
   // do something with the data
-  auto coordinates = BurtProto::decode<GpsCoordinates>(data, length, GpsCoordinates_fields);
+  // auto coordinates = BurtProto::decode<GpsCoordinates>(data, length, GpsCoordinates_fields);
+  GpsCoordinates coordinates;
   float angle = getAngle(coordinates);
   motor.moveTo(angle);
   // ...
