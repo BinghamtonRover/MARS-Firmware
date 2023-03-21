197e6d3cb727c4d75005060b39f1340673254a9e
Levi Lesches <levilesches@gmail.com>
2023-03-21 16:00:49 -0400

    Properly submoduled Firmware-Utilities

--- mars/mars.ino
+++ mars/mars.ino
@@ -1,5 +1,9 @@
-#include "tmc/src/BURT_TMC.h"
-#include "utils/src/BURT_utils.h"
+// #include "tmc/src/BURT_TMC.h"
+#include "src/utils/BURT_utils.h"
+
+void handler(const uint8_t* data, int length) { }
+BurtCan can(0, handler);
+BurtSerial serial(handler);
 
 void setup() { }
 void loop() { }
