634da457b5c95af18012a63877cc3db1c52ee88b
Levi Lesches <levilesches@gmail.com>
2023-03-21 16:06:58 -0400

    Added TMC library

--- mars/mars.ino
+++ mars/mars.ino
@@ -1,9 +1,10 @@
-// #include "tmc/src/BURT_TMC.h"
+#include "src/tmc/BURT_TMC.h"
 #include "src/utils/BURT_utils.h"
 
 void handler(const uint8_t* data, int length) { }
 BurtCan can(0, handler);
 BurtSerial serial(handler);
+StepperMotor motor(1, 2, 3, 4, 5, 6, 7, 8, 9, "Motor");
 
 void setup() { }
 void loop() { }
