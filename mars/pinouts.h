c79e51a216b06e186292f976555f7d207689820d
Levi Lesches <levilesches@gmail.com>
2023-05-23 19:04:22 -0400

    Updated submodules and regenerated Protobuf

---
+++ mars/pinouts.h
@@ -0,0 +1,73 @@
+#include "src/tmc/BURT_TMC.h"
+
+#define SPEED 0x000327e7
+#define ACCEL 0x00030d4d
+
+#define GPS_RX 16
+#define GPS_TX 17
+
+Gps gps(GPS_RX, GPS_TX);
+
+StepperMotorPins swivelPins = {
+	chipSelect: 10,
+	enable: 35,
+};
+
+StepperMotorConfig swivelConfig = {
+	name: "swivel",
+	current: 1500,
+	speed: SPEED,
+	accel: ACCEL
+	minLimit: -3 * PI/4,
+	maxLimit: 3 * PI/4,
+	isPositive: true,
+	gearboxRatio: 10.25,
+};
+
+LimitSwitch swivelLimitSwitch;
+
+StepperMotor swivel(swivelPins, swivelConfig, swivelLimitSwitch);
+
+// =============================================
+
+StepperMotorPins tiltPositivePins = {
+	chipSelect: 37,
+	enable: 34,
+};
+
+StepperMotorConfig tiltPositiveConfig = {
+	name: "tiltPos",
+	current: 2000,
+	speed: SPEED,
+	accel: ACCEL
+	minLimit: -10*PI,
+	maxLimit: 10*PI,
+	isPositive: true,
+	gearboxRatio: 8,
+};
+
+LimitSwitch tiltPositiveLimitSwitch;
+
+StepperMotor tiltPositive(tiltPositivePins, tiltPositiveConfig, tiltPositiveLimitSwitch);
+
+// =============================================
+
+StepperMotorPins tiltNegativePins = {
+	chipSelect: 36,
+	enable: 40,
+};
+
+StepperMotorConfig tiltNegativeConfig = {
+	name: "tiltNeg",
+	current: 2000,
+	speed: SPEED,
+	accel: ACCEL
+	minLimit: -10*PI,
+	maxLimit: 10*PI,
+	isPositive: false,
+	gearboxRatio: 8,
+};
+
+LimitSwitch tiltNegativeLimitSwitch;
+
+StepperMotor tiltNegative(tiltNegativePins, tiltNegativeConfig, tiltNegativeLimitSwitch);
--- mars/src/gps.pb.h
+++ mars/src/gps.pb.h
@@ -13,7 +13,23 @@
 typedef struct _GpsCoordinates {
     float latitude;
     float longitude;
+    float altitude;
 } GpsCoordinates;
+
+typedef struct _Orientation {
+    float x;
+    float y;
+    float z;
+} Orientation;
+
+typedef struct _RoverPosition {
+    bool has_gps;
+    GpsCoordinates gps;
+    bool has_orientation;
+    Orientation orientation;
+    bool has_base_station;
+    GpsCoordinates base_station;
+} RoverPosition;
 
 
 #ifdef __cplusplus
@@ -21,27 +37,62 @@ extern "C" {
 #endif
 
 /* Initializer values for message structs */
-#define GpsCoordinates_init_default              {0, 0}
-#define GpsCoordinates_init_zero                 {0, 0}
+#define GpsCoordinates_init_default              {0, 0, 0}
+#define Orientation_init_default                 {0, 0, 0}
+#define RoverPosition_init_default               {false, GpsCoordinates_init_default, false, Orientation_init_default, false, GpsCoordinates_init_default}
+#define GpsCoordinates_init_zero                 {0, 0, 0}
+#define Orientation_init_zero                    {0, 0, 0}
+#define RoverPosition_init_zero                  {false, GpsCoordinates_init_zero, false, Orientation_init_zero, false, GpsCoordinates_init_zero}
 
 /* Field tags (for use in manual encoding/decoding) */
 #define GpsCoordinates_latitude_tag              1
 #define GpsCoordinates_longitude_tag             2
+#define GpsCoordinates_altitude_tag              3
+#define Orientation_x_tag                        1
+#define Orientation_y_tag                        2
+#define Orientation_z_tag                        3
+#define RoverPosition_gps_tag                    1
+#define RoverPosition_orientation_tag            2
+#define RoverPosition_base_station_tag           3
 
 /* Struct field encoding specification for nanopb */
 #define GpsCoordinates_FIELDLIST(X, a) \
 X(a, STATIC,   SINGULAR, FLOAT,    latitude,          1) \
-X(a, STATIC,   SINGULAR, FLOAT,    longitude,         2)
+X(a, STATIC,   SINGULAR, FLOAT,    longitude,         2) \
+X(a, STATIC,   SINGULAR, FLOAT,    altitude,          3)
 #define GpsCoordinates_CALLBACK NULL
 #define GpsCoordinates_DEFAULT NULL
 
+#define Orientation_FIELDLIST(X, a) \
+X(a, STATIC,   SINGULAR, FLOAT,    x,                 1) \
+X(a, STATIC,   SINGULAR, FLOAT,    y,                 2) \
+X(a, STATIC,   SINGULAR, FLOAT,    z,                 3)
+#define Orientation_CALLBACK NULL
+#define Orientation_DEFAULT NULL
+
+#define RoverPosition_FIELDLIST(X, a) \
+X(a, STATIC,   OPTIONAL, MESSAGE,  gps,               1) \
+X(a, STATIC,   OPTIONAL, MESSAGE,  orientation,       2) \
+X(a, STATIC,   OPTIONAL, MESSAGE,  base_station,      3)
+#define RoverPosition_CALLBACK NULL
+#define RoverPosition_DEFAULT NULL
+#define RoverPosition_gps_MSGTYPE GpsCoordinates
+#define RoverPosition_orientation_MSGTYPE Orientation
+#define RoverPosition_base_station_MSGTYPE GpsCoordinates
+
 extern const pb_msgdesc_t GpsCoordinates_msg;
+extern const pb_msgdesc_t Orientation_msg;
+extern const pb_msgdesc_t RoverPosition_msg;
 
 /* Defines for backwards compatibility with code written before nanopb-0.4.0 */
 #define GpsCoordinates_fields &GpsCoordinates_msg
+#define Orientation_fields &Orientation_msg
+#define RoverPosition_fields &RoverPosition_msg
 
 /* Maximum encoded size of messages (where known) */
-#define GpsCoordinates_size                      10
+#define GpsCoordinates_size                      15
+#define Orientation_size                         15
+#define RoverPosition_size                       51
 
 #ifdef __cplusplus
 } /* extern "C" */
